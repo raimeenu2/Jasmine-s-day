b402d9f912a64aa88c19f9c2f8665334113768dc
Jasmine-Birthday <raimeenu254@gmail.com>
2026-04-16 21:31:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function showYes() {
+  document.getElementById("output").innerHTML =
+    "?? Calm choice. Happy Birthday, legend.";
+  document.getElementById("output").className = "";
+}
+
+function showYES() {
+  document.getElementById("output").innerHTML =
+    "?? CONFETTI MODE ACTIVATED (imagine it for now)";
+  document.getElementById("output").className = "";
+}
+
+function showOfcourse() {
+  document.getElementById("output").innerHTML =
+    "OFCOURSE! ?? If you know, you know.";
+  document.getElementById("output").className = "";
+}
+
+function showYup() {
+  document.getElementById("output").innerHTML =
+    "Hun ta kam krde meraa, hun ta mainu chintu te pintu da farak vi ratt gye ??";
+  document.getElementById("output").className = "yup-bg";
+}
